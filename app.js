619f7186743813719a13609952fcc70272e94a58
Пользователь <urinbaevg@gmail.com>
2023-01-04 15:12:20 +0500

    Project Habbit

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-const newElement = document.createElement('button');
-newElement.setAttribute('user-id', 11);
-newElement.classList.add('.button');
-newElement.innerText = 'Button';
-document.querySelector('.test').appendChild(newElement);
-
